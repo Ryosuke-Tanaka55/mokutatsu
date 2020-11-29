f27be28bfae227c5ef42a15fc6e92fb65cc5e5ed
Ryousuke-Tanaka <tanaka.r0815@gmail.com>
2020-11-29 09:04:15 +0900

    full-fledeged-login完成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,18 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ログイン後にユーザー情報ページにリダイレクトする
       log_in user # 引数で渡されたユーザーオブジェクトでログイン（sessionshelper参照）
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = "認証に失敗しました。"
       render :new
     end 
+  end
+
+  def destroy
+    # ログイン中の場合のみログアウト処理を行う
+    log_out if logged_in?
+    flash[:success] = "ログアウトしました。"
+    redirect_to root_url
   end
 end
