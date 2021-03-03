8419fd2d9feeb6cc91f67dba245480b2f73c1a90
Ryousuke-Tanaka <tanaka.r0815@gmail.com>
2021-03-03 10:30:23 +0900

    カレンダー日本語化_20210303

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,10 +1,37 @@
 $(function () {
+    function eventCalendar() {
+        return $('#calendar').fullCalendar({});
+        }
+
+        function clearCalendar() {
+        $('#calendar').html('');
+        }
+
   $('#calendar').fullCalendar({
+    titleFormat: 'YYYY年 M月',
+    dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+
     header: {
         left: 'prev,next today',
         center: 'title',
         right: 'month,agendaWeek,agendaDay'
     },
+
+    defaultTimedEventDuration: '03:00:00',
+    buttonText: {
+      prev: '前',
+      next: '次',
+      prevYear: '前年',
+      nextYear: '翌年',
+      today: '今日',
+      month: '月',
+      week: '週',
+      day: '日',
+    },
+    timeFormat: 'HH:mm',
+    eventColor: '#63ceef',
+    eventTextColor: '#000000',
+    
     defaultDate: new Date(),
     navLinks: true, // can click day/week names to navigate views
     selectable: true,
