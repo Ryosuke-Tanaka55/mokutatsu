43487f840e52eb2ce7d4037f0d528f9deb86c3d6
Ryousuke-Tanaka <tanaka.r0815@gmail.com>
2021-07-07 11:46:35 +0900

    DBサンプル追加_2021070704

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,67 @@ User.create!(name: "管理者",
   agreement: true,
   admin: true)
 
-60.times do |n|
+User.create!(name: "福田 周平",
+  email: "fukuda@email.com",
+  password: "password",
+  password_confirmation: "password",
+  agreement: true)
+
+User.create!(name: "宗 佑真",
+  email: "mune@email.com",
+  password: "password",
+  password_confirmation: "password",
+  agreement: true)
+
+User.create!(name: "吉田 正尚",
+  email: "yoshida@email.com",
+  password: "password",
+  password_confirmation: "password",
+  agreement: true)
+
+User.create!(name: "杉本 祐太郎",
+  email: "sugimoto@email.com",
+  password: "password",
+  password_confirmation: "password",
+  agreement: true)
+
+User.create!(name: "T-岡田",
+  email: "t-okada@email.com",
+  password: "password",
+  password_confirmation: "password",
+  agreement: true)
+
+User.create!(name: "安達 了一",
+  email: "adachi@email.com",
+  password: "password",
+  password_confirmation: "password",
+  agreement: true)
+
+User.create!(name: "スティーブン・モヤ",
+  email: "moya@email.com",
+  password: "password",
+  password_confirmation: "password",
+  agreement: true)
+
+User.create!(name: "紅林 幸太郎",
+  email: "kurebayashi@email.com",
+  password: "password",
+  password_confirmation: "password",
+  agreement: true)
+
+User.create!(name: "若月 健矢",
+  email: "wakatsuki@email.com",
+  password: "password",
+  password_confirmation: "password",
+  agreement: true)
+
+User.create!(name: "宮城 大弥",
+  email: "miyagi@email.com",
+  password: "password",
+  password_confirmation: "password",
+  agreement: true)
+  
+10.times do |n|
 name  = Faker::Name.name
 email = "sample-#{n+1}@email.com"
 password = "password"
